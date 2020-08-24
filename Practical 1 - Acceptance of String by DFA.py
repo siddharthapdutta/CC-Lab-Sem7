d5da33b2ac5d3a5c6dc8cdbdbe67978ea9385660
Siddhartha Dutta <siddharthadutta@protonmail.com>
2020-08-24 19:51:36 +0530

    Practical-1,2

---
+++ Practical 1 - Acceptance of String by DFA.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 19 11:40:26 2020
+Acceptance of String by DFA
+
+DFA1
+Accepts a string starting and ending with 'a'
+
+DFA2
+L = {a^n b^m | n mod 2 = 0, m >= 1}
+L = (aa)*b+
+
+@author: Siddhartha
+"""
+#%%
+def dfa1(string):
+    if string[0] == 'a' and string[-1] == 'a':
+        return True
+    return False
+
+
+# Transition Function for q0
+def q0(inp):
+    if inp == 'a':
+        dfa = 1
+    elif inp == 'b':
+        dfa = 3
+    else:
+        dfa = -1
+    return dfa
+
+# Transition Function for q1
+def q1(inp):
+    if inp == 'a':
+        dfa = 2
+    elif inp == 'b':
+        dfa = 4
+    else:
+        dfa = -1
+    return dfa
+
+# Transition Function for q2
+def q2(inp):
+    if inp == 'a':
+        dfa = 1
+    elif inp == 'b':
+        dfa = 3
+    else:
+        dfa = -1
+    return dfa
+
+# Transition Function for q3
+def q3(inp):
+    if inp == 'a':
+        dfa = 4
+    elif inp == 'b':
+        dfa = 3
+    else:
+        dfa = -1
+    return dfa
+
+def dfa2(string):
+    dfa = 0
+    for ch in string:
+        if dfa == 0: # Starting State
+            dfa = q0(ch)
+        elif dfa == 1:
+            dfa = q1(ch)
+        elif dfa == 2:
+            dfa = q2(ch)
+        elif dfa == 3:
+            dfa = q3(ch)
+        elif dfa == 4: # Reached Trap State
+            return False
+        else:
+            return False
+    if dfa == 3: # String Reached Final State
+        return True
+    return False
+
+
+if __name__ == '__main__':
+    string1 = 'abab'
+    if (dfa1(string1)):
+        print('Accepted!')
+    else:
+        print('Not Accepted!')
+    
+    string2 = 'aaaaaab'
+    if (dfa2(string2)):
+        print('Accepted!')
+    else:
+        print('Not Accepted!')
---
+++ Practical 2 - Implemention of Deterministic Finite Automata.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 19 11:25:01 2020
+Implemention of Deterministic Finite Automata
+@author: Siddhartha
+"""
+#%%
+def dfa(Q, S, q, transitions, F):
+    def string(string):
+        dfa_state = q
+        for ch in string:
+            print(ch, end=': q')
+            print(dfa_state, end=' -> q')
+            try:
+                dfa_state = transitions[dfa_state][ch]
+            except: # Undefined Transitions or Bad Character
+                return False
+            print(dfa_state)
+
+        if dfa_state in F:
+            return True
+        else:
+            return False
+    return string
+
+if __name__ == '__main__':
+    N = int(input("Enter number of states: "))
+    Q = ['q'+str(i) for i in range(N)]
+    print("States are:", *Q)
+    
+    q = int(input("Enter initial state: "))
+    F = list(map(int, input("Enter final state(s): ").strip().split(' ')))
+    S = list(input("Enter set of symbols: ").strip().split(' '))
+    
+    print("Enter transition table:")
+    print("State\t", end='')
+    print(*S, sep=' ')
+    
+    transitions = []
+    for state in Q:
+        print(state, end='\t') 
+        inp = list(map(int,input().strip().split(' ')))
+        transitions.append({key: value for key, value in zip(S, inp)})
+    
+    string = input('Enter input string: ').strip()
+    print('\n\nInput String: ', string)
+    print('Transitions')
+    automata = dfa(Q, S, q, transitions, F)
+    if (automata(string)):
+        print('\nAccepted!')
+    else:
+        print('\nNot Accepted!')    
